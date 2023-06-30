ea7ea8166378393f3058b107e43aadb9843d142d
keremakman272727 <52568414+keremakman272727@users.noreply.github.com>
2023-06-30 18:43:53 +0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BasketComponent } from './components/basket/basket.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutsComponent } from './components/layouts/layouts.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: LayoutsComponent,
+    children: [
+      {
+        path: "",
+        component: HomeComponent
+      },
+      {
+        path: "basket",
+        component: BasketComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
